5dc7fe7fb1be93b496df76002cef9fa31cb611e0
SP <spukk@tktk.ee>
2023-05-14 13:32:09 +0300

    add week9 code

---
+++ RKE132-week9/Program.cs
@@ -0,0 +1,55 @@
+﻿string folderPath = @"C:\data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+List<string> myShoppingList = new List<string>();
+
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"file {fileName} has been created");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+static List<string> GetItemsFromUser()
+{
+
+    List<string> userList = new List<string>();
+
+
+    while (true)
+    {
+        Console.WriteLine("Add an item (add)/ Exit (exit):");
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "add")
+        {
+            Console.WriteLine("Enter and item:");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem);
+        }
+        else
+        {
+            Console.WriteLine("Bye");
+            break;
+        }
+    }
+}
+
+Console.Clear();
+
+int listLenght = myShoppingList.Count;
+Console.WriteLine($" You have added {listLenght} items to your shopping list");
+
+int i = 1;
+foreach (string item in myShoppingList)
+{
+    Console.WriteLine($"{i}. {item}");
+    i++;
+}
